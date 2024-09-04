e11ebad4b83f573aac6cba8650e0ca344dcc21a6
Kevin Roan <kevinroan@proton.me>
2024-09-04 17:22:00 +0530

    reacthookforms implementation

--- components/userformModal.tsx
+++ components/userformModal.tsx
@@ -11,16 +11,27 @@ import {
 } from "@nextui-org/modal";
 import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
+import { ToastContainer, toast } from "react-toastify";
+import { Select, SelectOptions, SelectItem } from "@nextui-org/select";
+import "react-toastify/dist/ReactToastify.css";
 
 type Inputs = {
-  example: string;
-  exampleRequired: string;
+  email: string;
+  phone: string;
+  name: string;
 };
 
 type UserformModelProps = {
   isOpen: boolean;
-  onOpenChange: boolean;
+  onOpenChange: (open: boolean) => void; // Adjusted to match the correct type
 };
+
+const selectOptions = [
+  "Instagram",
+  "Youtube",
+  "Other Social Media",
+  "Word of Mouth",
+];
 
 const UserFormModel = ({ isOpen, onOpenChange }: UserformModelProps) => {
   const {
@@ -33,8 +44,24 @@ const UserFormModel = ({ isOpen, onOpenChange }: UserformModelProps) => {
 
   console.log(watch("example")); // watch input value by passing the name of it
 
+  const notify = () => {
+    toast("Our Team will contact you shortly");
+  };
+
   return (
     <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <form onSubmit={handleSubmit(onSubmit)}>
         <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
           <ModalContent>
@@ -45,16 +72,28 @@ const UserFormModel = ({ isOpen, onOpenChange }: UserformModelProps) => {
                   path
                 </ModalHeader>
                 <ModalBody>
-                  <Input placeholder="Enter Your Name" />
-                  <Input placeholder="Email" />
-                  <Input placeholder="Phone Number" />
-                  <Input placeholder="Enter Your Name" />
+                  <Input
+                    placeholder="Enter Your Name"
+                    {...register("name")}
+                    required
+                  />
+                  <Input placeholder="Email" {...register("email")} />
+                  <Input
+                    placeholder="Phone Number"
+                    {...register("phone")}
+                    required
+                  />
+                  <Select label="Where did you heard about us" size="sm">
+                    {selectOptions.map((item, index) => (
+                      <SelectItem key={index}>{item}</SelectItem>
+                    ))}
+                  </Select>
                 </ModalBody>
                 <ModalFooter>
-                  <Button color="danger" variant="light" onPress={onClose}>
+                  <Button color="danger" variant="light" onClick={onClose}>
                     Close
                   </Button>
-                  <Button color="danger" onPress={onClose}>
+                  <Button color="danger" onClick={notify}>
                     Get Started
                   </Button>
                 </ModalFooter>
